531c2d9c9a14dbafdc9a0fe1b963db10a3d2190a
Andrew Hodgson <ah9@me.com>
2014-03-10 14:25:47 +0000

    Add default log format param, improve comments, clean up cruft
    By adding a param for default format we achieve a couple of things:
    
      1. The module works without supplying a log format, as the `combined`
         format is built into nginx.
      2. We can plug in a custom log format from a client class.
    
    Since we're not using FreeBSD yet (for shame!) I've removed this case from the
    params class.
    
    Since the module is useless without params being set, and we only set params for
    a supported OS, I've moved all params under the case clause.
    
    I've also tidied them a bit an made puppet-lint happy re: variable scope.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,57 +1,42 @@
 # Class: nginx::params
 #
-#   class description goes here.
-#
-# Parameters:
-#
-# Actions:
-#
-# Requires:
+#   Data class containing operating-system-specific variables for use by other
+#   module classes.
 #
 # Sample Usage:
 #
+# class nginx::random_class {
+#   $myvar = $nginx::params::myvar
+#
+#   file { $myvar: }
+# }
+#
 class nginx::params {
-
-  $server_names_hash_bucket_size = 32
-
-  case $operatingsystem {
-    'debian',
+  case $::operatingsystem {
     'ubuntu': {
-        $package         = 'nginx-extras'
-        $service         = 'nginx'
-        $restart         = '/usr/sbin/nginx -t && /etc/init.d/nginx reload'
-        $hasrestart      = true
-        $hasstatus       = true
-        $etcdir          = '/etc/nginx'
-        $vdir            = "${etcdir}/sites-enabled"
-        $confd           = "${etcdir}/conf.d"
-        $threadcount     = $processorcount
-        $phpfpm_service  = 'php5-fpm'
-        $fastcgi_params  = '/etc/nginx/fastcgi_params'
-        $user            = 'www-data'
-        $default_webroot = '/var/www'
-      }
-    'freebsd': {
-        $package         = 'nginx'
-        $service         = 'nginx'
-        $restart         = '/usr/sbin/service nginx reload'
-        $hasrestart      = true
-        $hasstatus       = true
-        $etcdir          = '/usr/local/etc/nginx'
-        $vdir            = "${etcdir}/sites-enabled"
-        $confd           = "${etcdir}/conf.d"
-        $threadcount     = '4'
-        $user            = 'www'
-        $default_webroot = '/usr/local/www'
-      }
+      include ssl::params
+      $server_names_hash_bucket_size = 32
+      $default_ssl_path   = $ssl::params::ssl_path
+      $default_ssl_cert   = $ssl::params::ssl_cert_file
+      $default_ssl_key    = $ssl::params::ssl_key_file
+      $default_log_format = hiera('nginx::params::default_log_format', 'combined')
+      $default_webroot    = '/var/www'
+      $etcdir             = '/etc/nginx'
+      $fastcgi_params     = '/etc/nginx/fastcgi_params'
+      $hasrestart         = true
+      $hasstatus          = true
+      $package            = 'nginx-extras'
+      $phpfpm_service     = 'php5-fpm'
+      $restart            = '/usr/sbin/nginx -t && /etc/init.d/nginx reload'
+      $service            = 'nginx'
+      $threadcount        = $::processorcount
+      $user               = 'www-data'
+      $confd              = "${etcdir}/conf.d"
+      $vdir               = "${etcdir}/sites-enabled"
+    }
     default: {
-        warning( "Sorry, nginx module isn't built for ${operatingsystem} yet." )
+      fail("Sorry, nginx module doesn't support ${::operatingsystem} yet.")
     }
   }
-
-  include ssl::params
-  $default_ssl_path = $ssl::params::ssl_path
-  $default_ssl_cert = $ssl::params::ssl_cert_file
-  $default_ssl_key  = $ssl::params::ssl_key_file
-
 }
+
